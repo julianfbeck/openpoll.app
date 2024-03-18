479b60977b91f7fbdd72bb5b6297872b1c12673e
Julian Beck <ju-fa-beck@t-online.de>
2024-03-18 18:21:45 +0100

    Remove unused import statement

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import node from '@astrojs/node';
 import auth from 'auth-astro';
 import react from '@astrojs/react';
 
-import robotsTxt from 'astro-robots-txt';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://openpoll.app',
