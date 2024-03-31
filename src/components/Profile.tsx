d91afaab545b7b06441f215a7f7dd0d5450a8653
Julian Beck <ju-fa-beck@t-online.de>
2024-03-31 17:04:35 +0200

    Add @radix-ui/react-tooltip component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,84 +10,45 @@ import {
 } from '@/components/ui/table';
 import type { Poll } from '@/models/types';
 
-const invoices = [
-  {
-    invoice: 'INV001',
-    paymentStatus: 'Paid',
-    totalAmount: '$250.00',
-    paymentMethod: 'Credit Card'
-  },
-  {
-    invoice: 'INV002',
-    paymentStatus: 'Pending',
-    totalAmount: '$150.00',
-    paymentMethod: 'PayPal'
-  },
-  {
-    invoice: 'INV003',
-    paymentStatus: 'Unpaid',
-    totalAmount: '$350.00',
-    paymentMethod: 'Bank Transfer'
-  },
-  {
-    invoice: 'INV004',
-    paymentStatus: 'Paid',
-    totalAmount: '$450.00',
-    paymentMethod: 'Credit Card'
-  },
-  {
-    invoice: 'INV005',
-    paymentStatus: 'Paid',
-    totalAmount: '$550.00',
-    paymentMethod: 'PayPal'
-  },
-  {
-    invoice: 'INV006',
-    paymentStatus: 'Pending',
-    totalAmount: '$200.00',
-    paymentMethod: 'Bank Transfer'
-  },
-  {
-    invoice: 'INV007',
-    paymentStatus: 'Unpaid',
-    totalAmount: '$300.00',
-    paymentMethod: 'Credit Card'
-  }
-];
+import { Tooltip } from './ui/tooltip';
+import { Button } from './ui/button';
 
-export function UserProfile({ polls: polls }: { polls: Poll[] }) {
+export function UserProfile({ polls }: { polls: Poll[] }) {
   return (
-    <>
-      {polls.map((poll) => (
-        <div key={poll.id}>
-          <h2>{poll.question}</h2>
-          <Table>
-            <TableCaption>Options and Votes</TableCaption>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Option</TableHead>
-                <TableHead className="text-right">Votes</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {poll.options.map((option) => (
-                <TableRow key={option.id}>
-                  <TableCell>{option.option}</TableCell>
-                  <TableCell className="text-right">{option.votes}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TableCell>Total Votes</TableCell>
-                <TableCell className="text-right">
-                  {poll.options.reduce((acc, curr) => acc + curr.votes, 0)}
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </div>
-      ))}
-    </>
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Poll Question</TableHead>
+            <TableHead>Creation Date</TableHead>
+            <TableHead>Number of Options</TableHead>
+            <TableHead>Votes</TableHead>
+            <TableHead>Views</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {polls.map((poll) => (
+            <TableRow key={poll.id}>
+              <TableCell>
+                <a href={`/poll/${poll.shortId}`}>{poll.question}</a>
+              </TableCell>
+              <TableCell>{poll.timestamp}</TableCell>
+              <TableCell>{poll.options.length}</TableCell>
+              <TableCell>
+                {poll.options.reduce((acc, curr) => acc + curr.votes, 0)}
+              </TableCell>
+              <TableCell>{poll.views}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell>Total Polls</TableCell>
+            <TableCell>{polls.length}</TableCell>
+            <TableCell></TableCell> {/* Empty cell for alignment */}
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </div>
   );
 }
