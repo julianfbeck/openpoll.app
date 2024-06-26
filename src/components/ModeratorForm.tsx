ae128ecf58e655f98a9b2b2f047cf4e41278ed10
Julian Beck <ju-fa-beck@t-online.de>
2024-06-26 12:39:55 +0200

    refactor: Sort poll options based on votes and selected option

--- src/components/ModeratorForm.tsx
+++ src/components/ModeratorForm.tsx
@@ -74,13 +74,17 @@ export function ModeratorForm({ poll }: { poll: Poll }) {
           <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
         </span>
       </div>
-      {data?.options.map((item: PollOption) => (
+      {data?.options.sort((a, b) => {
+        if (a.id === poll.selectedPollOptionId) return -1;
+        if (b.id === poll.selectedPollOptionId) return 1;
+        return b.votes - a.votes;
+      }).map((item: PollOption) => (
         <div
           className={`flex flex-wrap items-start justify-between p-4 shadow ${data.selectedPollOptionId === item.id ? 'bg-green-100 border border-green-500' : ''}`}
           key={item.id}
         >
           <div className="text-sm font-normal break-words flex-auto min-w-0 mb-2 sm:mb-0">
-            {item.option}
+            {item.option} - {item.votes} votes
           </div>
           <div className="flex flex-row space-x-2 self-end">
             <Button onClick={() => handleSetAsCurrent(item.id)}>
