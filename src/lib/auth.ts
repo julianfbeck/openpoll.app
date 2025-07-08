bc8dbc43b624ab936fa5b5530f6e333ff47b8037
Julian Beck <ju-fa-beck@t-online.de>
2025-07-08 19:06:08 +0200

    Update package dependencies and fix baseURL in auth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,8 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 
 export const auth = betterAuth({
-  HERE I NEED TO UPDATE HTE LOCAL DEVELOPMENT _ CURRENTLS ITS BUGGY 
-  baseURL: "https://openpoll.pages.dev/",
+  // HERE I NEED TO UPDATE HTE LOCAL DEVELOPMENT _ CURRENTLS ITS BUGGY 
+  baseURL: "https://openpoll.app/",
   database: drizzleAdapter(db, {
     provider: 'sqlite',
     schema: {
